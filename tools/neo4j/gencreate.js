70051831e548e27d4cff245cfca118fcf8130145
Andreas Bergmeier <abergmeier@gmx.net>
2017-10-26 15:02:00 +0200

    Process capabilities
    Capabilities are now converted - still missing extension information.

--- tools/neo4j/gencreate.js
+++ tools/neo4j/gencreate.js
@@ -17,24 +17,67 @@ function buildLabel(text) {
 cypher = ""
 
 function createCypher(prefix, name, label, obj) {
-    cypher += "\nCREATE(" + prefix + name + ":" + label + JSON.stringify(obj) + ")"
+    let ser = JSON.stringify(obj)
+    // TODO: properly serialize
+    ser = ser.replace("\"name\":", "name: ")
+    ser = ser.replace("\"value\":", "value: ")
+    cypher += "\nCREATE(" + prefix + name + ":" + label + ser + ")"
 }
 
-function rowToCypher(prefix, label, row) {
-   let cells = row.querySelectorAll("td")
-    let name = cells[1].textContent
+function createEdge(lhs, rhs) {
+    cypher += "\nCREATE(" + lhs + ")-[:REQUIRES]->(" + rhs + ")"
+}
+
+function rowToCypher(prefix, label, row, depCol) {
+    let cells = row.querySelectorAll("td")
+    let name = cells[1].querySelector("strong").textContent
     createCypher(prefix, name, label, {
         name: name,
-        value: cells[0].textContent,
-    }) 
+        value: parseInt(cells[0].textContent, 10),
+    })
+
+    if (cells.length == 2)
+        return function() { 
+        }
+    
+    return function() {
+        let depText = cells[2].textContent.trim()
+        let deps = depText.split(",")
+        deps.forEach(function(depName) {
+            depName = depName.trim()
+            if (!depCol || !depName)
+                return
+
+            if (depCol == "caps")
+                createEdge(prefix + name, "C_" + depName)
+            else if(depCol == "deps")
+                createEdge(prefix + name, "C_" + depName)
+        })
+    }
 }
 
-(function() {
-    let heading = document.querySelector("#_a_id_source_language_a_source_language")
+[
+"#_a_id_capability_a_capability",
+"#_a_id_source_language_a_source_language",
+"#_a_id_execution_model_a_execution_model",
+"#_a_id_addressing_model_a_addressing_model",
+"#_a_id_memory_model_a_memory_model",
+].forEach(function(selector) {
+    let heading = document.querySelector(selector)
     let section = heading.parentElement
     let table = section.querySelector("table:first-of-type")
-    let tableHeader = table.querySelector("thead tr:first-of-type th")
-    let tableHeaderText = tableHeader.textContent
+    let tableHeaders = table.querySelectorAll("thead tr:first-of-type th")
+
+    let depCol = null
+    if (tableHeaders.length > 1) {
+        let depHeadingText = tableHeaders[1].textContent
+        if (depHeadingText == "Required Capability")
+            depCol = "caps"
+        else
+            depCol = "deps"
+    }
+
+    let tableHeaderText = tableHeaders[0].textContent
     let text = tableHeaderText.trim()
     let prefix = buildPrefix(text)
     if (prefix.length != 0)
@@ -43,9 +86,15 @@ function rowToCypher(prefix, label, row) {
     let label = buildLabel(text)
 
     let rows = table.querySelectorAll("tbody tr")
+    let linkers = []
     rows.forEach(function(row) {
-        rowToCypher(prefix, label, row)
-    })    
+        linkers.push(rowToCypher(prefix, label, row, depCol))
+    }) 
 
-    return cypher
-})()
+    linkers.forEach(function(linker) {
+        linker()
+    })
+})
+
+cypher
+
